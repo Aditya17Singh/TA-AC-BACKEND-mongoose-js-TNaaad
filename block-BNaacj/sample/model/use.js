437c73aebf0090956aaaecbf844be95f3cc5a306
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-08-17 12:28:10 +0530

    refactored

--- block-BNaacj/sample/model/use.js
+++ block-BNaacj/sample/model/use.js
@@ -3,8 +3,13 @@ var Schema = mongoose.Schema;
 
 
 var userSchema = new Schema({
-    password:{type: Number , minlength: 5, maxlength: 15},
-    createdAt: {type: Number , default: Date.now()}
+    name : String,
+    email:{type: String , lowercase: true},
+    age: {type:Number , default: 0},
+    fields: [String],
+    marks:[Number],
+    password:{type: String , minlength: 5, maxlength: 15},
+    createdAt: {type: Date , default: new Date()}
 })
 
 
